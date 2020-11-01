47ca7f4f96e740b6d45dd4f1580673dcf5bbd526
iss1977 <ioan.szucs@gmail.com>
2020-11-01 15:40:40 +0100

    First try. Working

---
+++ src/components/Main.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+  </div>
+
+  <div class="vote-wrapper">
+    <Vote username="My User" />
+  </div>
+
+    <Results username="My Results User"/>
+
+</template>
+
+<script>
+
+import Vote from './Vote';
+import Results from './Results';
+
+export default {
+  name: 'Main',
+  components:{Vote, Results},
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang ="scss" scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+/*
+* My Styles
+*/
+
+.vote-wrapper{
+  min-width: 300px;
+  max-width: 500px;
+  min-height: 300px;
+  max-height: 800px;
+  margin:auto;
+}
+
+
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="vote-window__main" @click ="favoriteTwoot(twoot.id)">
+        <div class="vote-window__question-label">
+            <p>Zeit fur noch eine Frage? </p>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name:"Results",
+    props:{
+        username:{
+            type: String,
+            required: true
+        }
+    },
+    methods:{
+        favoriteTwoot(id){
+            this.$emit('favourite',id);
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.twoot-item{
+    padding: 20px;
+    background-color: white;
+    border-radius: 5px;
+    border : 1px solid #DFE3E8;
+    box-sizing: border-box;
+    cursor: pointer;
+    transition: all 0.25s ease;
+
+    &:hover{
+        transform: rotate3d(1,1,1,45deg);
+        transform: scale(1.1,1.1);
+    }
+
+    .twoot-item__user{
+        font-weight: bold;
+    }
+
+} // end of .twoot-item
+
+
+
+.vote-window__main{
+
+    background-color: aquamarine;
+    padding: 1px;
+
+    .vote-window__question-label{
+        background-color: aliceblue;
+        margin : 20px;
+        border: 1px solid darkgray;
+    }
+
+    .vote-window__buttons-area{
+        margin: 10px;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+
+        button{
+            width : 30%;
+            max-width: 300px;
+            min-width: max-content;
+            padding: 10px;
+
+            &:hover{
+                transform: scale(1.1,1.1);
+            }
+        }
+    }
+
+}
+
+</style>
